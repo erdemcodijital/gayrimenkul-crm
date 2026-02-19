15e2a152a7bb6065e6103b26aa08d436ada19059
Arda Erdem <erdemstld@gmail.com>
2026-02-18 16:18:46 -0800

    FIX: Disable edit mode for production landing - Remove blue outlines and click handlers

--- app/d/[domain]/ProductionLandingPage.tsx
+++ app/d/[domain]/ProductionLandingPage.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { EditorProvider } from '@/contexts/EditorContext';
+import { useEffect } from 'react';
+import { EditorProvider, useEditor } from '@/contexts/EditorContext';
 import ClientLandingPage from './ClientLandingPage';
 
 interface Props {
@@ -8,10 +9,21 @@ interface Props {
   currentPage: any;
 }
 
+function ProductionWrapper({ agent, currentPage }: Props) {
+  const { setEditMode } = useEditor();
+  
+  // Force edit mode OFF for production
+  useEffect(() => {
+    setEditMode(false);
+  }, [setEditMode]);
+  
+  return <ClientLandingPage agent={agent} currentPage={currentPage} />;
+}
+
 export default function ProductionLandingPage({ agent, currentPage }: Props) {
   return (
     <EditorProvider>
-      <ClientLandingPage agent={agent} currentPage={currentPage} />
+      <ProductionWrapper agent={agent} currentPage={currentPage} />
     </EditorProvider>
   );
 }
